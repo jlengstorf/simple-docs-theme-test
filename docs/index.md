dfbece7179ed4bdb6bb230c61c1bb233d459cb11
Jason Lengstorf <jason@lengstorf.com>
2019-02-08 20:01:31 -0800

    feat: demo of using the simple docs theme

---
+++ docs/index.md
@@ -0,0 +1,5 @@
+---
+title: My Rad Docs Site
+---
+
+This is my rad docs site!
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My Sweet Docs Site',
+    description: 'Docs for using my rad thing.'
+  },
+  __experimentalThemes: ['gatsby-theme-simple-docs']
+};
